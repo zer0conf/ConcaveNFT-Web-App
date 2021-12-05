a974980cbbcea57cbeb684e0f64cad0e2bbd6837
wetcircle <65508703+wetcircle@users.noreply.github.com>
2021-12-04 21:13:21 -0500

    design changes

--- src/App.js
+++ src/App.js
@@ -76,14 +76,14 @@ export const StyledLogo = styled.img`
 
 export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
-  border: 4px dashed var(--secondary);
+  border: 4px solid var(--secondary);
   background-color: var(--accent);
   border-radius: 100%;
   width: 200px;
-  @media (max-width: 900px) {
-    width: 250px;
+  @media (min-width: 900px) {
+    width: 300px;
   }
-  @media (max-width: 1000px) {
+  @media (min-width: 1000px) {
     width: 300px;
   }
   transition: width 0.5s;
@@ -373,7 +373,7 @@ function App() {
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg
               alt={"example"}
-              src={"/config/images/example.gif"}
+              src={"/config/images/example1.gif"}
               style={{ transform: "scaleX(-1)" }}
             />
           </s.Container>
